6b7f508436d25dcc4f7bfd2a734d4079c771061f
Kibeom Nam <105034250+namkibeom@users.noreply.github.com>
2023-09-16 17:37:15 +0900

    [level 3] Title: 거스름돈, Time: 755.81 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12907. 거스름돈/거스름돈.py
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.py
@@ -1,7 +1,3 @@
 def solution(n, money):
-    dp = [1] + [0] * n #1은 money에서의 자기자신 횟수+1
-    for m in money: #m이라는 money 축 기준으로 횟수 늘어남
-        for i in range(m, n+1):
-            dp[i] = (dp[i] + dp[i-m]) % 1000000007 #dp[i-m]의 경우 현재 m 횟수랑 이전 m에 따라 더해진 횟수가 반영됨
-    return dp[n]
-#시간복잡도 = O(n), 공간복잡도 = O(n)
+    answer = 0
+    return answer
